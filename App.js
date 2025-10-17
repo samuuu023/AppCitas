ef52a99e023664e58f99c5a502a1ef1dfdd41d73
Samuu <samuuu23@outlook.com>
2025-10-16 22:16:11 -0600

    Subir app

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Layout, Input, ButtonRounded } from "./Components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout title="Contacto">
+      <Input label="Email:" placeholder="name@mail.com" type="email-address" />
+      <Input label="Asunto:" placeholder="Consulta" />
+      <Input label="Mensaje:" placeholder="Escribe aquí..." lines={4} />
+    </Layout>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
